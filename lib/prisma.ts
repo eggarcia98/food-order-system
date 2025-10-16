79353cc489aee6c65777f9b22ab10643070c5317
Erick Garcia <eggarcia9814@gmail.com>
2025-10-16 16:23:17 +1000

    chore: remove unused '@neondatabase/serverless' dependency from project

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,6 @@ import { neonConfig, Pool } from '@neondatabase/serverless'
 
 
 const connectionString = `${process.env.DATABASE_URL}`;
-console.log('Connection String:', connectionString);
 
 const adapter = new PrismaNeon({ connectionString });
 export const prisma = new PrismaClient({ adapter });
