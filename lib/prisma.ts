bc6f3433608045ac23a65b04bbca1c2a596e4085
Erick Garcia <eggarcia9814@gmail.com>
2025-10-16 16:28:05 +1000

    chore: remove unused imports from prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { neonConfig, Pool } from '@neondatabase/serverless'
 
 
 const connectionString = `${process.env.DATABASE_URL}`;
