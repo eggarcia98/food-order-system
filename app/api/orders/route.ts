1c3a019f5f1272ed03ead2d74078f0e3fea5dbed
Erick Garcia <eggarcia9814@gmail.com>
2025-10-16 16:49:50 +1000

    feat: implement POST endpoint for creating orders with client and dish details

---
+++ app/api/orders/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+export const runtime = "edge";
+
+export async function POST(request: Request) {
+    try {
+        const { client, dishes, comments } = await request.json();
+
+        const order = await prisma.order.create({
+            data: {
+                customer: {
+                    connectOrCreate: {
+                        where: {
+                            phone_number: client.phoneNumber,
+                        },
+                        create: {
+                            first_name: client.firstName,
+                            last_name: client.lastName,
+                            nationality_id: client.nationalityId,
+                            phone_number: client.phoneNumber,
+                            // email: client.email,
+                        },
+                    },
+                },
+                order_code: `ORD-BRI${Date.now()}`,
+                order_item: {
+                    create: dishes.map((dish: any) => ({
+                        dish_id: dish.id,
+                        quantity: dish.quantity,
+                    })),
+                },
+                
+                comments,
+            },
+            include: {
+                client: true,
+                dishes: true,
+            },
+        });
+
+        return NextResponse.json(order);
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
