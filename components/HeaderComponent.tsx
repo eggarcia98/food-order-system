5e849b594f396da4b812f7cb89395f7974f5525b
Erick Garcia <eggarcia9814@gmail.com>
2025-11-20 09:56:15 +1000

    Refactor HeaderComponent: replace Order button with a link and add mobile menu button

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -2,12 +2,18 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { Menu } from "lucide-react";
 
 export default function HeaderComponent() {
     return (
         <header className="bg-white border-b border-brand-blue font-bungee font-bold text-xs sm:text-[16px] ">
-            <div className="max-w-6xl mx-auto px-6  flex items-center justify-between">
-                <Link href="/" className="flex items-center gap-3">
+            <div className="max-w-6xl mx-auto px-6 grid grid-cols-3 items-center md:flex sm:justify-between ">
+                {/* Mobile menu button */}
+                <button className="md:hidden  justify-self-start">
+                    <Menu size={"20"} className="text-lg" />
+                </button>
+
+                <Link href="/" className="justify-self-center">
                     <Image
                         src="/media/logo.png"
                         alt="Los Guayacos Restaurant"
@@ -17,8 +23,11 @@ export default function HeaderComponent() {
                     />
                 </Link>
 
-                <nav aria-label="Main Navigation">
-                    <ul className="flex items-center gap-6 underline underline-offset-4 ">
+                <nav
+                    aria-label="Main Navigation"
+                    className="flex gap-6 justify-self-end"
+                >
+                    <ul className="hidden md:flex items-center gap-6 underline underline-offset-4 ">
                         <li>
                             <Link
                                 href="/"
@@ -35,15 +44,13 @@ export default function HeaderComponent() {
                                 Contact
                             </Link>
                         </li>
-                        <li>
-                            <button
-                                className="btn-brand-blue p-2 font-semibold rounded-lg disabled:cursor-not-allowed transition shadow-lg hover:shadow-xl "
-                                onClick={() => { }}
-                            >
-                                Order Now
-                            </button>
-                        </li>
                     </ul>
+                    <Link
+                        className="btn-brand-blue p-2 font-semibold rounded-lg disabled:cursor-not-allowed transition shadow-lg hover:shadow-xl "
+                        href={"/new_order"}
+                    >
+                        Order Now
+                    </Link>
                 </nav>
             </div>
         </header>
