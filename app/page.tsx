8986e7ab8ef8e44a50513bfefc2b70565873d95b
Erick Garcia <eggarcia9814@gmail.com>
2025-11-19 20:21:30 +1000

    Implement New Order page with customer registration and order submission functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,14 @@
 // app/page.tsx
 "use client";
 
-import NewOrderModal from "@/components/NewOrderModal";
 import Link from "next/link";
-import { useState } from "react";
 
 // Dis is a comment
 export default function Page() {
-    const [openNewOrderModal, setOpenNewOrderModal] = useState(false);
-
-    const handleOpenNewOrderModal = (value: any) => {
-        setOpenNewOrderModal(value);
-    };
+  
 
     return (
         <div className="bg-background h-dvh flex items-center justify-center">
-            <NewOrderModal
-                openNewOrderModal={openNewOrderModal}
-                setOpenNewOrderModal={handleOpenNewOrderModal}
-            />
             <Link
                 className="btn-brand-blue p-4 font-semibold rounded-lg disabled:cursor-not-allowed transition shadow-lg hover:shadow-xl "
                 href={"/new_order"}
